648dbd7ce088b95b472b3c732439b888ae35fa77
lunalachique <onesingrave@gmail.com>
2024-09-08 00:05:20 +0300

    Days_done

---
+++ python_files/day05.py
@@ -0,0 +1,17 @@
+print("Which type of coffee are you")
+print()
+espresso = input("Are you in the morning energized and ready to conquer the world?")
+latte = input("Are you slow and steady, easing into the morning?")
+mocha = input("In the morning you are with a burst of creativity and inspiration?")
+ice = input("In the morning you are relaxed and taking things as they come?")
+print()
+if espresso == "Yes":
+  print("You are espresso!! ✨")
+if latte == "Yes":
+  print("You are latte!! ❤️")
+if mocha == "Yes":
+  print("You are mocha!! 😝")
+if ice == "Yes":
+  print("You are Ice Coffee!! 😊")
+else:
+  print("Tea?.. Is that you?! 😘")
