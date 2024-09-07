8588bcee031e05d8ce57b8cfeda909620b7e5e95
lunalachique <onesingrave@gmail.com>
2024-09-07 17:20:02 +0300

    Izpildita 1 diena

--- python_files/day01.py
+++ python_files/day01.py
@@ -1,1 +1,8 @@
-print()
+print("Diana Lagocka")
+print("2024. gada 1 Septembris")
+print("""I am signing up for Replit's 100 days of Python challenge! 
+      I will make sure to spend some time every day coding along, for a minimum 10 minutes a day.
+      I will be using Replit, an amazing online IDE so I can do this from my phone 
+      wherever I happen to be. No excuses for not conding from the middle of the field!""")
+print("I am feeling 🙌✨🏅!!!")
+print("You can follow my progress at github.com/lunalachique")
